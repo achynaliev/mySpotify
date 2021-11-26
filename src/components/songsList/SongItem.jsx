09002d64d4ab4660bd47d901c3f1ac46542c3ec5
Atai Chynaliev <chynaliev.ae@gmail.com>
2021-11-26 17:54:01 +0600

    styling for main page

--- src/components/songsList/SongItem.jsx
+++ src/components/songsList/SongItem.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import "./songs.css";
 
-const SongItem = ({ song }) => {
-  return <div>song</div>;
+const SongItem = ({ song, index }) => {
+  console.log(song);
+  return (
+    <div className="songListItem">
+      <div className="firstItemListItem">
+        <h4 className="listItemTexth4">{index + 1}</h4>
+      </div>
+      <div className="secondItemListItem">
+        <img className="coverListItem" src={song.songCover} alt=""></img>
+        <h4 className="listItemTexth4">{song.songName}</h4>
+      </div>
+      <div className="thirdItemListItem">
+        <h4 className="listItemTexth4">{song.artistName}</h4>
+      </div>
+      <div className="forthItemListItem">
+        <h4 className="listItemTexth4gray">{song.album}</h4>
+      </div>
+      <div className="fifthItemListItem">
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default SongItem;
--- src/components/songsList/SongList.jsx
+++ src/components/songsList/SongList.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { fireDB } from "../../base.js";
 import { collection, getDocs } from "firebase/firestore";
 import SongItem from "./SongItem";
+import "./songs.css";
 
 const SongList = () => {
   const [songs, setSongList] = React.useState([]);
@@ -29,8 +30,25 @@ const SongList = () => {
   return (
     <>
       <div className="songListCont">
+        <div className="songListHeader">
+          <div className="firstItemListItem">
+            <h4 className="listItemTexth4gray">#</h4>
+          </div>
+          <div className="secondItemListItem">
+            <h4 className="listItemTexth4gray">Title</h4>
+          </div>
+          <div className="thirdItemListItem">
+            <h4 className="listItemTexth4gray">Artists</h4>
+          </div>
+          <div className="forthItemListItem">
+            <h4 className="listItemTexth4gray">Album</h4>
+          </div>
+          <div className="fifthItemListItem">
+            <h4 className="listItemTexth4gray">_</h4>
+          </div>
+        </div>
         {songs.map((song, index) => {
-          return <SongItem key={index} song={song}></SongItem>;
+          return <SongItem key={index} song={song} index={index}></SongItem>;
         })}
       </div>
     </>
